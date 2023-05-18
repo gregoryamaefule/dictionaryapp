284746dbaebe00d00034c97d89abe45e887ff554
innovin <innovinanuonye@gmail.com>
2023-05-18 14:01:24 +0100

    fixed handling non-existent words

--- src/PartOfSpeech.jsx
+++ src/PartOfSpeech.jsx
@@ -1,44 +1,38 @@
-import { useState } from 'react'
-import dictionaryLogo from './assets/iconoir_book.png'
-// import oval from './assets/ovalwhite.png'
-import ovalpurp from './assets/Oval.png'
-import halfmoon from './assets/iconoir_half-moon.png'
-import halfmoondark from './assets/iconoir_half-moon-dark.png'
-import iconoir_search from './assets/iconoir_search.png'
-import audioIcon from './assets/Group4.png'
+
 import './App.css'
 
-export default function PartOfSpeech({isLightMode, fontSpecified, meaning}){
-    if(meaning == undefined){
-      return null
+export default function PartOfSpeech({isLightMode, fontSpecified, meaning}) {
+    if (meaning == undefined) {
+        return null
     }
-    const listitems =  meaning.definitions.map( item => {
-      return(
-        <>
-          <li className='li' style={isLightMode ? null : {color: 'white'}}>{item.definition}</li>
-          <span className='ex'>{item.example}</span>
-        </>
-      )
-    }
+    const listitems = meaning.definitions.map((item, index) => {
+            return (
+                <>
+                    <li key={`li-${index}`} className='li' style={isLightMode ? null : {color: 'white'}}>{item.definition}</li>
+                    <span key={`span-${index}`} className='ex'>{item.example}</span>
+                </>
+            )
+        }
     )
-    const synonyms = meaning.synonyms.length != 0 ? 
-    <p className='syn'>
-      <span>Synonyms</span>
-      <span className='sy'>{meaning.synonyms.map( (item) => <span style={{paddingRight: '8px'}}>{`${item} `}</span> )}</span>
-    </p> : null
-  
-    
-    return(
-      <div className='POS'>
-      <div className='hd'>
-      <span className='ps' style={isLightMode ? null : {color: 'white'}}>{meaning.partOfSpeech}</span>
-      <hr></hr>
-      </div>
-        <span className='MNG'>Meaning</span>
-      <ul>
-        {listitems}
-      </ul>
-      {synonyms}
-      </div>
+    const synonyms = meaning.synonyms.length > 0 ?
+        <p className='syn'>
+            <span>Synonyms</span>
+            <span className='sy'>{meaning.synonyms.map((item, index) => <span key={index}
+                                                                              style={{paddingRight: '8px'}}>{`${item} `}</span>)}</span>
+        </p> : null
+
+
+    return (
+        <div className='POS'>
+            <div className='hd'>
+                <span className='ps' style={isLightMode ? null : {color: 'white'}}>{meaning.partOfSpeech}</span>
+                <hr></hr>
+            </div>
+            <span className='MNG'>Meaning</span>
+            <ul>
+                {listitems}
+            </ul>
+            {synonyms}
+        </div>
     )
-  }
+}
--- src/ResultHeader.jsx
+++ src/ResultHeader.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import dictionaryLogo from './assets/iconoir_book.png'
-// import oval from './assets/oval.png'
-import halfmoon from './assets/iconoir_half-moon.png'
-import halfmoondark from './assets/iconoir_half-moon-dark.png'
-import iconoir_search from './assets/iconoir_search.png'
 import audioIcon from './assets/Group4.png'
 import './App.css'
 
+// eslint-disable-next-line react/prop-types
 export default function ResultHeader ({isLightMode, fontSpecified, word, phonetic}){
     return(
       <div className='resultHeader'>
